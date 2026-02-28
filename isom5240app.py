6b154f7dd94c1ad23a88eb7e52d3fc80b66a252d
GZCN-Leo <leo.rene.cn@gmail.com>
2026-02-28 17:42:28 +0800

    Move age classification logic into age_classifier function
    Refactor age classification logic to be within the age_classifier function.

--- isom5240app.py
+++ isom5240app.py
@@ -9,18 +9,20 @@ def age_classifier():
     age_classifier = pipeline("image-classification",
                           model="prithivMLmods/Age-Classification-SigLIP2")
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-print(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+    image_name = "middleagedMan.jpg"
+    image_name = Image.open(image_name).convert("RGB")
+    
+    # Classify age
+    age_predictions = age_classifier(image_name)
+    print(age_predictions)
+    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+   
+    st.write(age_predictions[0]['label'])
 
 def main():
     st.header("Title: Age Classification using ViT")
     age_classifier()
-    st.write(age_predictions[0]['label'])
+    
   
 # main part
 if __name__ == "__main__":
